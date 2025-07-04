efebd82bbf763791328fbf8a318e4aaa6626ca71
sajal8987 <166208187+sajal8987@users.noreply.github.com>
2025-07-04 10:57:15 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,16 @@
+//let score = true;
+
+//console.log(typeof score);
+
+
+//let newScore = Number(score);
+//console.log(newScore)
+
+
+// "33" = 33;
+// "33abc" = nan;
+// true = 1; false = 0;
+
+let score = 1;
+let convertboolean = Boolean(score);
+console.log(convertboolean);
