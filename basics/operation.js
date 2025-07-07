1ea4f4c571254ddbdbbc987356737a1963c41baa
sajal8987 <166208187+sajal8987@users.noreply.github.com>
2025-07-07 18:29:06 +0000

    operation and variable

---
+++ basics/operation.js
@@ -0,0 +1,11 @@
+console.log(2+2);       
+console.log(1 +2 +"2");
+console.log("2"+ 1+2);
+console.log(+true)
+console.log(+"")
+
+4
+32
+212
+1
+0
---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+const Username = "atul";
+var useremail = "atul@gmail.com"
+let salary = 15000;
+
+console.table([Username, useremail, salary])
+
+
+var newuseremail = "atul123@gmail.com";
+var useremail = newuseremail
+
+console.log(useremail)
