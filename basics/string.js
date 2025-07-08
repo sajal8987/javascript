1e4bb7d5fbd7c699e818e30ebfca299bbb6260a4
sajal8987 <166208187+sajal8987@users.noreply.github.com>
2025-07-08 13:25:51 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,12 @@
+const name = 'hitesh';
+const repoCount = 50;
+
+console.log(name +repoCount)
+
+const gameName = new String('hitesh');
+//console.log(`${gameName} ${repoCount} ${gameName}` )
+
+
+//some methods of string
+console.log(gameName[1]);
+console.log(gameName.length);
