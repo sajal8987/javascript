9e0909f43d4406c49cb9ea1861529452d037a212
sajal8987 <166208187+sajal8987@users.noreply.github.com>
2025-07-16 04:48:39 +0000

    date and number,math

---
+++ basics/dates.js
@@ -0,0 +1,14 @@
+//let mydate = new Date();
+//console.log(mydate.toString());
+//console.log(mydate.toDateString())
+
+//let mycreateddate = new Date("01-02-2003");
+//console.log(mycreateddate.toDateString())
+
+//let timestamp1 = Date.now();
+// console.log(timestamp1)
+// console.log(mycreateddate.getTime())
+
+//console.log(Math.floor(Date.now()/1000))
+
+
---
+++ basics/numbers_and_maths.js
@@ -0,0 +1,25 @@
+// const score = 400;
+// const balance =new Number(95.83222);
+
+// // console.log(balance)
+
+// // console.log(balance.toString())
+// // console.log(typeof(balance))
+
+// //console.log(balance.toFixed(1))on
+// //console.log(balance.toPrecision(3))
+
+// const hundreds = 100000;
+// console.log(hundreds.toLocaleString('en-IV'));
+
+
+//......................................Maths........................................
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.2))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(5.8))
+// console.log(Math.min(4,5,6,1))
+// console.log(Math.max(4,9,88,5))
+//console.log((Math.random()*10)+1) // to avoid 0 case
+//console.log(Math.floor(Math.random()*10)+1)
